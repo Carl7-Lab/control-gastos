04028123fa19392463789129acc43f622ec294da
Carl7 <carlosmiguel7cb@gmail.com>
2022-05-13 16:11:13 -0500

    Header.jsx creado

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import NuevoPresupuesto from "./NuevoPresupuesto"
+
+function Header() {
+  return (
+    <header>
+      <h1>Planificador de Gastos</h1>
+      <NuevoPresupuesto />
+    </header>
+  )
+}
+
+export default Header
